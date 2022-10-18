f6c832acba782de1b83f16eef1ce5a22ddee0e22
David T Laszlo <l.davidtamas@gmail.com>
2022-10-18 18:47:33 +0200

    pixi particles added

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ import * as PIXI from "pixi.js";
 import Player from "./player.js";
 import Zombie from "./zombie.js";
 import Spawner from "./spawner.js";
-import {zombies} from "./globals.js";
-//import Matter from "matter-js";
+import { zombies } from "./globals.js";
 
 const canvasSize = 512;
 const canvas = document.getElementById("mycanvas");
@@ -22,49 +21,50 @@ initGame();
 async function initGame() {
   try {
     console.log("Loading...");
-   await loadAssets();
+    await loadAssets();
     console.log("Loaded");
-    
-    let player = new Player({app});
-    let zSpawner = new Spawner ({app, create: () => new Zombie({ app, player})});
+
+    let player = new Player({ app });
+    let zSpawner = new Spawner({
+      app,
+      create: () => new Zombie({ app, player })
+    });
 
     let gameStartScene = createScene("Click to Start");
     let gameOverScene = createScene("Game Over");
     app.gameStarted = false;
 
-app.ticker.add((delta) => {
-  gameOverScene.visible = player.dead;
-  gameStartScene.visible = !app.gameStarted;
-  if(app.gameStarted === false) return;
-  player.update(delta);
-  zSpawner.spawns.forEach((zombie) => zombie.update(delta));
-  bulletHitTest({
-    bullets:player.shooting.bullets,
-    zombies:zSpawner.spawns,
-    bulletRadius:8,
-    zombieRadius:16
+    app.ticker.add((delta) => {
+      gameOverScene.visible = player.dead;
+      gameStartScene.visible = !app.gameStarted;
+      if (app.gameStarted === false) return;
+      player.update(delta);
+      zSpawner.spawns.forEach((zombie) => zombie.update(delta));
+      bulletHitTest({
+        bullets: player.shooting.bullets,
+        zombies: zSpawner.spawns,
+        bulletRadius: 8,
+        zombieRadius: 16
+      });
     });
-});
-
-    
-  } catch(error) {
+  } catch (error) {
     console.log(error.message);
     console.log("Load filed");
   }
 }
 
-function bulletHitTest({bullets,zombies,bulletRadius,zombieRadius}) {
-  bullets.forEach(bullet => {
-    zombies.forEach( (zombie,index) => {
+function bulletHitTest({ bullets, zombies, bulletRadius, zombieRadius }) {
+  bullets.forEach((bullet) => {
+    zombies.forEach((zombie, index) => {
       let dx = zombie.position.x - bullet.position.x;
       let dy = zombie.position.y - bullet.position.y;
-      let distance = Math.sqrt(dx*dx + dy*dy);
+      let distance = Math.sqrt(dx * dx + dy * dy);
       if (distance < bulletRadius + zombieRadius) {
-        zombies.splice(index,1);
+        zombies.splice(index, 1);
         zombie.kill();
       }
-    })
-  })
+    });
+  });
 }
 
 function createScene(sceneText) {
@@ -72,7 +72,7 @@ function createScene(sceneText) {
   const text = new PIXI.Text(sceneText);
   text.x = app.screen.width / 2;
   text.y = 0;
-  text.anchor.set(0.5,0);
+  text.anchor.set(0.5, 0);
   sceneContainer.zIndex = 1;
   sceneContainer.addChild(text);
   app.stage.addChild(sceneContainer);
@@ -84,7 +84,7 @@ function startGame() {
 }
 
 async function loadAssets() {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     zombies.forEach((z) => PIXI.Loader.shared.add(`assets/${z}.json`));
     PIXI.Loader.shared.add("assets/hero_male.json");
     PIXI.Loader.shared.add("bullet", "assets/bullet.png");
@@ -92,7 +92,7 @@ async function loadAssets() {
     PIXI.Loader.shared.onComplete.add(resolve);
     PIXI.Loader.shared.onError.add(reject);
     PIXI.Loader.shared.load();
-  })
+  });
 }
 
 document.addEventListener("click", startGame);
