58b9ac88a60c209b73b476f2d5db0c4d725d874a
RagingSquirrel <l.davidtamas@gmail.com>
2022-10-18 16:27:36 +0000

    Update player.js and shooting.js

--- shooting.js
+++ shooting.js
@@ -27,11 +27,14 @@ export default class Shooting {
         
         this.bullets.forEach((b) => this.app.stage.addChild(b));
         
-        const bullet = new PIXI.Graphics();
-        bullet.position.set(this.player.position.x, this.player.position.y);
-        bullet.beginFill(0x0000ff,1);
-        bullet.drawCircle(0,0,this.bulletRadius);
-        bullet.endFill();
+        const bullet = new PIXI.Sprite(
+            PIXI.Loader.shared.resources["bullet"].texture
+            );
+            bullet.anchor.set(0.5);
+            bullet.scale.set(0.2);
+            bullet.position.set(this.player.position.x, this.player.position.y);
+            bullet.rotation = this.player.rotation;
+        
         let angle = this.player.rotation - Math.PI / 2;
         bullet.velocity = new Victor(
             Math.cos(angle),
